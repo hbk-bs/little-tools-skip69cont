64c9399af588a6c2fc80231edea5a045c37933bb
skip69cont <148953741+skip69cont@users.noreply.github.com>
2023-11-29 14:30:27 +0100

    painttool
    frist own painttool

--- sketch.js
+++ sketch.js
@@ -1,6 +1,22 @@
+const saveButton = document.querySelector
+    ("button#save");
+const colorInput = document.querySelector
+    ("input#color");
+console.log(colorInput);
+
+let paintColor = '#ff3456'
+colorInput.value = paintColor;
+
 function setup() {
-    createCanvas(500, 500);
-   
+   const canvas = createCanvas(500,500);
+   canvas.parent("sketch");
+    background (200);
+    saveButton.addEventListener("click",()=>{
+        save("image.png");  
+   });
+   colorInput.addEventListener("input", ()=>{
+        paintColor = colorInput.value;
+   });
 }
    
 function draw() {
@@ -8,6 +24,7 @@ function draw() {
 }
 
 function mouseDragged(){
+    fill(paintColor);
     circle(mouseX,mouseY, 10);
     line(pmouseX, pmouseY, pmouseX, pmouseY);
     for (let i = 0; i < 100; i++) {
@@ -17,6 +34,10 @@ function mouseDragged(){
             mouseY+random(-10,10));
         
     }
+}
+
+function buttonHandler(){
+    
 }
 
 function keyPressed(){
